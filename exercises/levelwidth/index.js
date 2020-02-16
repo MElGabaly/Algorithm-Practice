b8fda91ebe23be84c859f3a37482dbcaf535c322
Mo <Gabalyma@gmail.com>
2020-02-16 14:52:30 -0800

    90%

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,21 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+  let counters = [0];
+  const arr = [root, "endOfRow"];
+  while (arr.length > 1) {
+    const node = arr.shift();
+    // ... takes all the children and pushes them in the end of array
+    if (node === "endOfRow") {
+      counters.push(0);
+      arr.push("endOfRow");
+    } else {
+      arr.push(...node.children);
+      counters[counters.length - 1]++;
+    }
+  }
+  return counters;
+}
 
 module.exports = levelWidth;
